279e8e64aaa77340c675f6954f599dc0fc971b95
Julia <granovskaya.97@mail.ru>
2022-06-24 21:16:35 +0300

    Made homework

--- Program.cs
+++ Program.cs
@@ -1,15 +1,88 @@
-﻿// Console.Write("Imput number: ");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// Console.Write("Imput number: ");
 // int i = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine(i*i);
 
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// Console.Write("Imput number_1: ");
+// int number_1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput number_2: ");
+// int number_2 = Convert.ToInt32(Console.ReadLine());
 
+// if (number_1 == number_2*number_2)
+//     Console.WriteLine("a = "+ number_1 +", b = "+ number_2 +" -> yes");
+// else
+//     Console.WriteLine("a = "+ number_1 +", b = "+ number_2 +", -> no");
 
-Console.Write("Imput number_1: ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Imput number_2: ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// Console.Write("Number of day: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day == 1)
+//     Console.WriteLine("Monday");
+// if (day == 2)
+//     Console.WriteLine("Tuesday");
+// if (day == 3)
+//     Console.WriteLine("Wednesday");
+// if (day == 4)
+//     Console.WriteLine("Thursday");
+// if (day == 5)
+//     Console.WriteLine("Friday");
+// if (day == 6)
+//     Console.WriteLine("Saturday");
+// if (day == 7)
+//     Console.WriteLine("Sunday");
+// else
+//     Console.WriteLine("Incorrect number");
 
-if (number_1 == number_2*number_2)
-    Console.WriteLine("a = "+ number_1 +", b = "+ number_2 +" -> yes");
-else
-    Console.WriteLine("a = "+ number_1 +", b = "+ number_2 +", -> no");
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write("First number: " );
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 > number2)
+//     {Console.WriteLine("a = "+ number1 +", b = "+ number2 +" -> max = "+ number1 +"");
+//     }
+// else if (number1 == number2)
+// {
+//     Console.WriteLine("a = "+ number1 +", b = "+ number2 +" -> equal");
+// }
+// else Console.WriteLine("a = "+ number1 +", b = "+ number2 +" -> max = "+ number2 +"");
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+// Console.Write("First number: " );
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Third number: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int max = number1;
+// if (number2 > max)
+//     {
+//     max = number2;
+//     }
+// else if (number3 > max)
+//     {
+//     max = number3;
+//     }
+// Console.WriteLine(""+ number1 +" "+ number2 +" "+ number3 +" -> "+ max +"");
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.Write("Number: " );
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 2 == 0)
+//     {
+//         Console.WriteLine(""+ number +" -> yes");
+//     }
+// else Console.WriteLine(""+ number +" -> no");
+
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// Console.Write("Number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = 0;
+// Console.WriteLine(""+ a +" -> ");
+// while (b < a)
+//     {Console.WriteLine(b = b - a);
+//     b = b + 1;
+//     };
+
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
