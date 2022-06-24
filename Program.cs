d4d952a627897ee1b32ac837029a0f4e88a00958
Julia <granovskaya.97@mail.ru>
2022-06-24 19:32:39 +0300

    Решили 2 задачи

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.Write("Imput number: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(i*i);
+
+
+
+Console.Write("Imput number_1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number_2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 == number_2*number_2)
+    Console.WriteLine("a = "+ number_1 +", b = "+ number_2 +" -> yes");
+else
+    Console.WriteLine("a = "+ number_1 +", b = "+ number_2 +", -> no");
